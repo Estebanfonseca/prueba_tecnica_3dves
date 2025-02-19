cabd300a516ff61ab1ffa191a3f1758a4cf771cc
Estebanfonseca <estebanp57@hotmail.com>
2025-02-19 17:45:21 -0500

    se crean los servicios, controladores de crear y consultar libros  y se conectan con la base dev datos para su funcionamiento

--- api_leelo_todo/src/books/services/books.service.ts
+++ api_leelo_todo/src/books/services/books.service.ts
@@ -1,32 +1,80 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBookDto } from '../dto/create-book.dto';
-import { UpdateBookDto } from '../dto/update-book.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Book } from '../entities/book.entity';
+import { Author } from '../entities/author.entity';
+import { Category } from '../entities/category.entity';
 import { Repository } from 'typeorm';
 
 @Injectable()
 export class BooksService {
   constructor(
     @InjectRepository(Book) private readonly bookRepository: Repository<Book>,
+    @InjectRepository(Author)
+    private readonly authorRepository: Repository<Author>,
+    @InjectRepository(Category)
+    private readonly categoryRepository: Repository<Category>,
   ) {}
-  async create(createBookDto: CreateBookDto) {
-    return 'This action adds a new book';
-  }
 
-  async findAll() {
-    return await this.bookRepository.find();
+  async create(createBookDto: CreateBookDto) {
+    let author = await this.authorRepository.findOne({
+      where: { name: createBookDto.authorName },
+    });
+
+    if (!author) {
+      author = this.authorRepository.create({ name: createBookDto.authorName });
+      await this.authorRepository.save(author);
+    }
+
+    let category = await this.categoryRepository.findOne({
+      where: { name: createBookDto.categoryName },
+    });
+
+    if (!category) {
+      category = this.categoryRepository.create({
+        name: createBookDto.categoryName,
+      });
+      await this.categoryRepository.save(category);
+    }
+
+    const book = this.bookRepository.create({
+      name: createBookDto.name,
+      author,
+      category,
+      basePrice: createBookDto.basePrice,
+    });
+    return await this.bookRepository.save(book);
   }
 
   async findOne(id: number) {
-    return `This action returns a #${id} book`;
+    return await this.bookRepository.findOneBy({ id });
   }
 
-  async update(id: number, updateBookDto: UpdateBookDto) {
-    return `This action updates a #${id} book`;
-  }
+  async findByAuthorOrCategory(categoryId?: number, authorId?: number) {
+    const query = this.bookRepository.createQueryBuilder('book');
 
-  async remove(id: number) {
-    return `This action removes a #${id} book`;
+    if (categoryId) {
+      query.innerJoinAndSelect(
+        'book.category',
+        'category',
+        'category.id = :categoryId',
+        { categoryId },
+      );
+    }
+
+    if (authorId) {
+      query.innerJoinAndSelect(
+        'book.author',
+        'author',
+        'author.id = :authorId',
+        { authorId },
+      );
+    }
+
+    if (!authorId && !categoryId) {
+      return this.bookRepository.find();
+    }
+
+    return query.getMany();
   }
 }
