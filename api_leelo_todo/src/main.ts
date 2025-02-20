61c86d5d03e9da7fc57a4f66d4094eb5a018976f
Estebanfonseca <estebanp57@hotmail.com>
2025-02-19 19:57:58 -0500

    se agrega documentacion con swagger

--- api_leelo_todo/src/main.ts
+++ api_leelo_todo/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,15 @@ async function bootstrap() {
     }),
   );
 
+  const config = new DocumentBuilder()
+    .setTitle('Api Leelo Todo')
+    .setDescription('Api para la gestion de la tienda')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
